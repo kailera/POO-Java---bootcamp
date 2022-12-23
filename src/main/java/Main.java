27874221afdb612c38196b0bf5696ecd68feb64b
kailera <ferreirasamuel459@gmail.com>
2022-12-23 12:37:27 -0300

    Add curso and mentoria class and create objects

---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+import br.com.domain.Curso;
+import br.com.domain.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitle("Curso Java");
+        curso1.setDescription("Curso Java Iniciante");
+        curso1.setWorkload(10);
+
+        Curso curso2 = new Curso();
+        curso2.setTitle("JS Curso");
+        curso2.setDescription("Curso Iniciante de JavaScript");
+        curso2.setWorkload(10);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitle("Mentoria Java");
+        mentoria1.setDescription("Mentoria de Java para inciantes");
+        mentoria1.setDate(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria1);
+    }
+}
